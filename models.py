fbe584bd9caf7a42d116b29b1afb1c17f2c300a2
jejorm <jejorm8@gmail.com>
2022-01-20 17:37:20 -0500

    models_tweets

---
+++ models.py
@@ -0,0 +1,26 @@
+from datetime import date, datetime
+from uuid import UUID
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserBase(BaseModel):
+	user_id: UUID = Field(...)
+	email: EmailStr = Field(...)
+
+
+class User(UserBase):
+	first_name: str = Field(..., min_length=1, max_length=50)
+	last_name: str = Field(..., min_length=1, max_length=50)
+	birthday: date | None =  Field(default=None)
+
+
+class UserLogin(UserBase):
+	password: str = Field(..., min_length=8, max_length=64)
+
+
+class Tweet(BaseModel):
+	tweet_id: UUID = Field(...)
+	content: str = Field(..., min_length=1, max_length=256)
+	created_at: datetime = Field(default=datetime.now())
+	updated_at: datetime | None = Field(default=None)
+	by: User = Field(...)
